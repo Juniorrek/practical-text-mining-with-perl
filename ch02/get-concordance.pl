e285be0e01174e5291ccf60a7ddea9c03b58e2ed
aaronwells <chacewells@gmail.com>
2016-05-02 12:35:28 -0700

    something about a concordance

---
+++ ch02/get-concordance.pl
@@ -0,0 +1,37 @@
+use v5.22;
+
+my $target = qr/\b($ARGV[0])\b/i;
+my $radius = $ARGV[1];
+my $filename = $ARGV[2];
+
+open FILE, $filename or die 'File not found';
+
+$/ = ""; # paragraph mode for first while loop
+my $width = 2 * $radius; # width of exract w/out target
+
+# first while loop
+while (<FILE>) {
+    s/\r//g;
+    chomp;
+    s/\n/ /g; # replace newlines by spaces
+    s/--/ -- /g; # add spaces around dashes
+
+    # second while loop
+    while ( $_ =~ /$target/g ) { # $target is already case-insensitive
+        my $match = $1;
+        my $pos = pos($_);
+        my $start = $pos - $radius - length($match);
+
+        my ($extract, $len);
+        if ($start < 0) {
+            $extract = substr($_, 0, $width + $start + length($match));
+            $extract = (' ' x -$start) . $extract;
+            $len = length($extract);
+        } else {
+            $extract = substr($_, $start, $width + length($match));
+        }
+
+        # print the next concordance line
+        print "$extract\n";
+    }
+}
