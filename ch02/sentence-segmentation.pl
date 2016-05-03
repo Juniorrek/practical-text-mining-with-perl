1c3d6c60d6d635e0c906e79e509deb63529518f5
aaronwells <chacewells@gmail.com>
2016-05-02 18:21:26 -0700

    added parser to tokenize "A Christmas Carol " sentences

--- ch02/sentence-segmentation.pl
+++ ch02/sentence-segmentation.pl
@@ -2,7 +2,26 @@
 use feature 'say';
 
 # first attempt
+# $test = "Testing. one, two, three.  Hello!  What?";
+
+# while ( $test =~ /([A-Z].*?[\.\?!])\s*[A-Z]/g ) { # upper case only?
+# while ( $test =~ /([A-Z].*?[.?!])\s*[A-Z]/g ) { # still not quite there. 1) Search starts at 'e' in "Hello! " rather than backing up first and 2) expects last bit to be [A-Z], even at the end of the string
+# while ( $test =~ /([A-Z].*?[.?!])\s*[A-Z]/g ) { # we'd rather avoid managing the position if Perl can do it for us
+#     $loc = pos($test) -= 1;
+#     say $1;
+# }
+# say substr($test, $loc);
+
 $test = "Testing. one, two, three.  Hello!  What?";
-while ( $test =~ /([A-Z].*?[\.\?!])\s*[A-Z]/g ) {
-    say $1;
+$buffer = '';
+
+while ( $test =~ /(\s*[^.?!]+[.?!])/g ) {
+    $match = $1;
+    # Note use of the Perl variable $'
+    if ( $' =~ /^\s*[A-Z]|^\s*$/ ) {
+        print "$buffer$match\n";
+        $buffer = '';
+    } else {
+        $buffer .= $match;
+    }
 }
