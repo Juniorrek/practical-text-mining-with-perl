a28aea439b03d00890aeb5ac59ec736b570723bd
Aaron Wells <chacewells@gmail.com>
2016-05-02 20:37:37 -0700

    tokenize-sentences.pl complete with correction for mid-parentheses sentence ending; will probably refine after completing problem 2.7

--- ch02/tokenize-sentences.pl
+++ ch02/tokenize-sentences.pl
@@ -4,33 +4,37 @@ use feature 'say';
 open FILE, '<christmas-carol.txt' or die('File not found');
 
 $/ = ""; # paragraph mode
+$quotes = qr/["']{0,2}/;
 $regex = qr/
             (
-                ["']{0,2} [a-zA-Z] [^.?!]*
-                ["']{0,2} [.?!] ["']{0,2} \s*
+                \)? \s* $quotes [[:alpha:]]
+                [^.?!]*?
+                $quotes [.?!] $quotes \s*
             )
          /x;
 
 while (<FILE>) {
     s/\r//g;
-    s/\u{feff}//g;
     chomp;
-    s/\n/ /g;       # replace newlines by space
-    s/--/ -- /g;    # buffer dashes with space
-    s/(Mrs?\.)/\1/g;# remove title periods
+    s/\n/ /g;           # replace newlines by space
+    s/--/ -- /g;        # buffer dashes with space
+    s/(Mrs?)\./\1/g;    # remove title periods
 
     $buffer = '';
     while ( /$regex/g ) {
+        $p = $_;
         $match = $1;
-        if ( $' =~ /^"?[A-Z]/ ) {   # check for capital letter
-            say $buffer, $match; # print sentence
+        if ( $match =~ /\(.*?$/ or $' =~ /^\)/ ) {
+            $buffer .= $match;
+        } elsif ( $' =~ /^"?[A-Z]/ ) {   # check for capital letter
+            say $buffer, $match;    # print sentence
             $buffer = '';
         } else {
             $buffer .= $match;
             if ($' =~ /^\w*$/ ) { # check for end of paragraph
-                say $buffer;    # print sentence
+                say $buffer;      # print sentence
             }
         }
     }
-    say;
+    say '';
 }
