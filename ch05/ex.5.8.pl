7ddb963f37530b718a3d02aef12f84dcba936e74
aaronwells <chacewells@gmail.com>
2016-05-25 06:52:39 -0700

    finished problem 5.7

---
+++ ch05/ex.5.8.pl
@@ -0,0 +1,12 @@
+# 5.8 Rewrite output 5.3 so that the matrix M is defined by a vector that contains its rows, not its columns
+> M = matrix(c(19,27,24,33,9,5,0,0,7,10,28,17,13,11,0,3,22,55,35,32,0,0,0,0,1,4,3,1,2,0,0,0), nrow=8, ncol=4, byrow=T)
+> M
+     [,1] [,2] [,3] [,4]
+[1,]   19   27   24   33
+[2,]    9    5    0    0
+[3,]    7   10   28   17
+[4,]   13   11    0    3
+[5,]   22   55   35   32
+[6,]    0    0    0    0
+[7,]    1    4    3    1
+[8,]    2    0    0    0 
