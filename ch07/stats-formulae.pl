3fa0dad742b18adf5ac7fd4564d1fa99bf0398bc
Aaron Wells <aaron.wells@expeditors.com>
2016-06-14 13:44:21 -0700

    more formulae

--- ch07/stats-formulae.pl
+++ ch07/stats-formulae.pl
@@ -40,7 +40,7 @@ sub z_score {
 }
 
 =head2 Sample Correlation Coefficient
-=item assumes @x == @y
+=item assumes $#x == $#y
 =cut
 sub corr_coeff {
     my @x = @{ shift() };
@@ -50,9 +50,40 @@ sub corr_coeff {
 
     ( 1 / ($n - 1) )
   * sum map {
-          ( $x[$_] - $mean_x )
-        / $std_x
-        * ( $y[$_] - $mean_y )
-        / $std_y
+          ( $x[$_] - $mean_x ) / $std_x
+        * ( $y[$_] - $mean_y ) / $std_y
     } 0 .. $#x;
+}
+
+=head2 equivalence between correlation and cosine
+=item assumes $#x == $#y
+=cut
+sub cos_via_correlation {
+    my @x = @{ shift() };
+    my @y = @{ shift() };
+    my( $sum_xy, $sumx2, $sumy2 );
+    
+    for (0 .. $#x) {
+        $sum_xy += $x[$_] * $y[$_];
+        $sumx2 += $x[$_] ** 2;
+        $sumy2 += $y[$_] ** 2;
+    }
+    ($sum_xy) / (sqrt( $sum_x) * sqrt( $sum_y ))
+}
+
+=head2 
+=item assumes $#x == $#y
+=cut
+sub covariance {
+    my($x,$y) = @_;
+    my $n = @$x;
+    my($mean_x, $mean_y) = map { sum(@$_) / $n } $x, $y;
+    ( 1 / (n - 1) ) * sum( map { ($x->[$_] - $mean_x) * ($y->[$_] - $mean_y) } 1 .. $#{$x} );
+}
+
+=head2 equivalence: covariance == correlation
+=cut
+sub correlation_via_covariance {
+    my( $x, $y ) = @_;
+    covariance($x, $y) / ( std_dev(@$x) * std_dev(@$y) )
 }
