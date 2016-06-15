e182cd4200b2da338c3190e14f37a95b29290178
Aaron Wells <chacewells@gmail.com>
2016-06-14 19:35:39 -0700

    made setup.sh portable

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
+this_computer="$(uname)"
+if [ "x$this_computer" = "xDarwin" ];then
+    dev_folder=~/Programming
+elif [ "x$this_computer" = "xLinux" ];then
+    dev_folder=~/dev
+fi
+
 export POE_PATH="$PWD/data/poe-68-stories"
-export ENRONMAIL_PATH=~/Programming/data/enronmail
+export ENRONMAIL_PATH="$dev_folder/data/enronmail"
